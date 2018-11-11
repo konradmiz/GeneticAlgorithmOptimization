9cab6ab9b395795dcf16753434136238bc9bc401
Konrad Miziolek <5964679+konradmiz@users.noreply.github.com>
2018-11-11 13:07:03 -0800

    Add files via upload

--- Running GA.py
+++ Running GA.py
@@ -8,54 +8,70 @@ Created on Thu Nov  1 09:09:43 2018
 import sys
 sys.path.insert(0, "C:/Users/Konrad/Desktop/GeneticAlgorithm")
 import firstGA as GA
-
+from collections import Counter
 import numpy as np
 import pandas as pd
+import operator
 from scipy.spatial import distance_matrix
 from scipy.stats import mode
 from math import log
 
-#bike_id = list(range(num_bikes))
+df = pd.read_csv("C:/Users/Konrad/Desktop/GeneticAlgorithm/df.csv")
+dist_m = pd.read_csv("C:/Users/Konrad/Desktop/GeneticAlgorithm/dist.csv")
+num_bikes = df.shape[0] - 1
+#test_df = pd.DataFrame([[0,0], [0,0.5], [0,1], [1,1], [1,0]])
+#test_df.columns = ['x', 'y']
+#test_dist_m = distance_matrix(test_df, test_df)
+
+#df = pd.DataFrame({'x':np.random.random(size = num_bikes), 'y':np.random.random(size = num_bikes)})
+#shop = pd.DataFrame([[0,0]], columns = ['x', 'y'])
+#df = df.append(shop)
+#df
+#dist_m
+#dist_m = distance_matrix(df, df)
 
 class Environment():
-
-    #global self.algs
        
-    def __init__(self, num_bikes = 150, max_dist = 50, pop_size = 30, reproducing_frac = 0.10):
+    def __init__(self, dist_m = dist_m, bikes_df = df, num_bikes = 150, max_dist = 50, 
+                 pop_size = 30, reproducing_frac = 0.10):
         self.algs = []  
+        self.all_sols = []
+        self.gen_best = []
+        
+        self.dist_m = dist_m
+        self.df = bikes_df
         
         self.num_bikes = num_bikes
         self.max_dist = max_dist
         self.pop_size = pop_size
         self.num_reproducing = int(pop_size * reproducing_frac)
-           
-        df = pd.DataFrame({'x':np.random.random(size = num_bikes), 'y':np.random.random(size = num_bikes)})
-        shop = pd.DataFrame([[0,0]], columns = ['x', 'y'])
-        self.df = df.append(shop)
-        
-        self.dist_m = distance_matrix(self.df, self.df)
-        
-        test_df = pd.DataFrame([[0,1], [1,1], [1,0], [0,0.5], [0,0]])
-        self.test_dist_m = distance_matrix(test_df, test_df)
         
         while len(self.algs) < self.pop_size:
-            new_alg = GA.Algorithm(parents = False)
+            new_alg = GA.Algorithm(parents = False, i = 0)
 
             if new_alg.get_distance(self.dist_m) <= self.max_dist: # feasible solution
                 self.algs.append(new_alg)
         
     def find_parents(self):
-        max_val = sorted([a.get_fitness() for a in self.algs], reverse = True)[self.num_reproducing-1]
-        self.algs = [a for a in self.algs if a.get_fitness() >= max_val] # overwriting the algs to just the fittest
-        #return potential_parents
-        
-        
-    
-    def next_generation(self):
+        self.algs = sorted(self.algs, key = operator.attrgetter('dist'))
+        self.algs = sorted(self.algs, key = operator.attrgetter('solution_size'), reverse = True)[:self.num_reproducing-1]
+
+        #self.algs = sorted(self.algs, key = operator.attrgetter('dist'))
+
+        #print([(round(a.solution_size, 3), round(a.dist, 3)) for a in self.algs])
+        #self.algs = self.algs
+        #min_repr_fitness = sorted([a.get_fitness() for a in self.algs], reverse = True)[self.num_reproducing-1]
+        #self.algs = [a for a in self.algs if a.get_fitness() >= min_repr_fitness] # overwriting the algs to just the fittest
+            
+    def next_generation(self, i):
         pot_par = self.algs[:]
         
-        total_fitness = sum([p.get_fitness() for p in pot_par])
-        repr_prob = [p.get_fitness()/total_fitness for p in pot_par]
+        #total_fitness = sum([p.get_fitness() for p in pot_par])
+        #repr_prob = [p.get_fitness()/total_fitness for p in pot_par]
+        num_par = len(pot_par)
+        repr_prob = sorted(list(range(0, num_par)), reverse = True)
+        repr_prob_sum = sum(repr_prob)
+        repr_prob = [r/repr_prob_sum for r in repr_prob]
         
         while len(self.algs) < self.pop_size:
             first_par = np.random.choice(pot_par, p = repr_prob)
@@ -64,7 +80,7 @@ class Environment():
             while first_par == second_par:
                 second_par = np.random.choice(pot_par, p = repr_prob)
             
-            new_alg = GA.Algorithm(parents = [first_par, second_par])
+            new_alg = GA.Algorithm(parents = [first_par, second_par], i = i)
             
             if new_alg.get_distance(self.dist_m) <= self.max_dist: # feasible solution
                 self.algs.append(new_alg)
@@ -75,29 +91,35 @@ class Environment():
         avg_distance = round(sum(a.get_distance(self.dist_m) for a in self.algs)/num_algs, 3)
         
         best_fitness = max([a.get_fitness() for a in self.algs])
-        
+        best_fitness_dist = round(min([a.get_distance(self.dist_m) for a in self.algs if a.get_fitness() == best_fitness]), 3)
         med_fitness = mode([len(a.solution) for a in self.algs], axis = None)[0]
         array_list = [a.solution for a in self.algs if len(a.solution) == med_fitness]
         
-        avg_entropy = calculate_entropy(array_list)/med_fitness
+        avg_entropy = calculate_entropy(array_list)/(med_fitness - 2) # start and end are fixed  \
         
-        print("Iteration: {}\t AvgFitness: {}\t Best Fitness: {}\t AvgDistance: {}\t Avg Entropy:{}"
-              .format(i, avg_fitness, best_fitness, avg_distance, avg_entropy))
+        print("Iteration: {}\t AvgFitness: {}\t Best Fitness: {}\t BestDistance: {}\t Avg Entropy:{}"
+              .format(i, avg_fitness, best_fitness, best_fitness_dist, avg_entropy))
         
     def run_simulation(self, num_iter):
         
         self.summarize_generations(i = 0)
-        
+        self.unique_sols = np.zeros(num_iter)       
         for i in range(num_iter):
+            current_solutions = [a.solution for a in self.algs]
+            self.all_sols.append(current_solutions)
+            current_all_sols = [a for b in self.all_sols for a in b]
+            self.unique_sols[i] = len(Counter(tuple(a) for a in current_all_sols))
             self.find_parents()
-            self.next_generation()
+            self.next_generation(i)
             self.summarize_generations(i+1)
+            self.gen_best.append(self.get_best())
 
     def get_best(self):
         best_fitness = max([a.get_fitness() for a in self.algs])
-        best_sol = [a for a in self.algs if a.get_fitness() == best_fitness]
+        min_dist = min([a.get_distance(self.dist_m) for a in self.algs if a.get_fitness() == best_fitness])
+        best_sol = [a for a in self.algs if (a.get_fitness() == best_fitness) and (a.get_distance(self.dist_m) == min_dist)][0]
         
-        return best_sol
+        return best_sol, min_dist, best_sol.solution
 
 def calculate_entropy(array_list): # adapted from https://stackoverflow.com/questions/15450192/fastest-way-to-compute-entropy-in-python
     ent = 0.
@@ -105,7 +127,7 @@ def calculate_entropy(array_list): # adapted from https://stackoverflow.com/ques
     if len(array_list) < 2:
         return 0
     
-    for i in range(len(array_list)):
+    for i in range(len(array_list[0])):
         entry = [a[i] for a in array_list]
         value,counts = np.unique(entry, return_counts=True)
         probs = counts / len(entry)
@@ -113,16 +135,54 @@ def calculate_entropy(array_list): # adapted from https://stackoverflow.com/ques
         for i in probs:
             ent -= i * log(i, 2)
 
-    return ent
+    return round(ent, 3)
 
-one_trial = Environment(pop_size = 30, max_dist = 10)
-one_trial.run_simulation(num_iter = 100)
+one_trial = Environment(num_bikes = num_bikes, pop_size = 90, max_dist = 800, reproducing_frac = 0.1,
+                        dist_m = dist_m, bikes_df = df)
+one_trial.run_simulation(num_iter = 50)
+#[(a.get_fitness(), a.dist) for a in one_trial.algs]
+one_trial.gen_best
+min_dist = min([a[1] for a in one_trial.gen_best])
+best_sol = [a[2] for a in one_trial.gen_best if a[1] == min_dist][0]
 
-#test_list = [[0,1], [1,0]]
-
-#calculate_entropy(test_list)
-
-bike_route = one_trial.get_best()[0].solution
-
+np.savetxt("C:/Users/Konrad/Desktop/GeneticAlgorithm/route.csv", best_sol, delimiter = ",")
 one_trial.df.to_csv("C:/Users/Konrad/Desktop/GeneticAlgorithm/bike_locations.csv")
-np.savetxt("C:/Users/Konrad/Desktop/GeneticAlgorithm/route.csv", bike_route, delimiter = ",")
+
+#one_trial.get_best()[0].parents
+#one_trial.get_best()[0].solution
+
+#one_trial.get_best()[0].solution_size
+#one_trial.get_best()[0].swaps
+#one_trial.get_best()[0].mutations
+#one_trial.get_best()[0].trial_no
+
+#[list(a.solution) for a in one_trial.algs]
+#one_trial.unique_sols.tolist()
+#one_trial.all_sols
+#one_trial.get_best()[0].recom
+
+#[a.parents for a in one_trial.algs]
+#[a.inversions for a in one_trial.algs]
+#[a.solution for a in one_trial.algs]
+#one_trial.algs[49].parents
+#one_trial.algs[49].swaps
+#one_trial.algs[49].solution
+#one_trial.algs[49].swaps
+#one_trial.algs[49].inversions
+
+
+#best_sol
+
+
+#one_trial.get_best()[0].get_distance(one_trial.dist_m)
+#test_df
+#test_dist_m
+#one_trial.get_best()[1].solution
+
+#[(a.solution, a.id) for a in one_trial.get_best()]
+#[a.inversions for a in one_trial.algs if a.id == 'f8c1c439-e203-11e8-97db-aeb6d0d9cb77']
+#import cProfile
+#import re
+#cProfile.run(one_trial.run_simulation(num_iter = 5))
+#cProfile.run('re.compile("foo|bar")')
+#cProfile.run('re.compile("one_trial.run_simulation(num_iter = 20)")')
--- firstGA.py
+++ firstGA.py
@@ -1,11 +1,17 @@
 import numpy as np
+import uuid
 
-num_bikes = 150
+num_bikes = 117
 class Algorithm():
-    def __init__(self, parents):
+    def __init__(self, parents, i):
+        
+        self.id = str(uuid.uuid1())
+        self.trial_no = i
         
         self.mutations = []
+        self.swaps = []
         self.insertions = []
+        self.inversions = []
         
         if not parents:
             self.random_generation()
@@ -17,6 +23,7 @@ class Algorithm():
                 self.random_generation()        
             else:
                 self.mutation()
+                self.inversion()
                 self.solution_size = len(self.solution) - 2
         
         self.new_gene()
@@ -27,19 +34,18 @@ class Algorithm():
         if solution_size is not None:
             self.solution_size = solution_size
         else:
-            self.solution_size = np.random.randint(low = 0, high = num_bikes)
+            self.solution_size = np.random.randint(low = 0, high = num_bikes+1)
         
         self.solution = np.random.choice(list(range(1, num_bikes+1)), size = self.solution_size, replace = False)
         self.solution = np.insert(self.solution, obj = 0, values = 0)
         self.solution = np.insert(self.solution, obj = len(self.solution), values = 0)
-        
+       
     def recombination(self, parents):
-        
-        max_trials = 100
+        max_trials = 50
         num_trials = 0
         
         while True:
-            self.recom_spot = np.random.randint(low=1, high=len(parents[0].solution))
+            self.recom_spot = np.random.randint(low=1, high=len(parents[0].solution)-1)
             self.first_bit = parents[0].solution[:self.recom_spot]
             self.second_bit = parents[1].solution[self.recom_spot:]
         
@@ -47,33 +53,61 @@ class Algorithm():
             num_trials += 1
         
             if len(new_sol) == len(set(new_sol)) + 1: # success - no duplicate values except 0
+                self.recom = 1
                 return new_sol
             if num_trials == max_trials:
                 return
+            
+    def inversion(self, inversion_rate = 0.5):
+        if np.random.uniform() <= inversion_rate:
+            inversion_pt = np.random.choice(list(range(1, len(self.solution) - 1)))
+            
+            self.first_part = self.solution[:inversion_pt]
+            self.second_part = np.flipud(self.solution[inversion_pt:len(self.solution)-1])
+            self.second_part = np.concatenate((self.second_part, [0]))
+            # keep the solution until the recombination point, invert the remaining bits
+            
+            self.solution = np.concatenate((self.first_part, self.second_part))
+            self.inversions.append([self.first_part, self.second_part])
         
-    def mutation(self, mutation_rate = 0.2):
-        # which sites are not in the solution        
-        choices = list(set(self.solution).symmetric_difference(set(list(range(num_bikes))))) 
+    def mutation(self, mutation_rate = 0.8):
+         # which sites are not in the solution        
+        #choices = list(set(list(range(1, num_bikes+1))) - set(self.solution))
 
         for i in range(1, len(self.solution) - 1): # can't mutate first or last bit
-            if (np.random.uniform() <= mutation_rate) and (len(choices) != 0):
-                new_gene = np.random.choice(choices)
-                self.mutations.append([self.solution[i], new_gene])
-                self.solution[i] = new_gene
-
-    def new_gene(self, new_gene_rate = None):
-        
-        if new_gene_rate is None:
-            new_gene_rate = 10/len(self.solution)
-
-        choices = list(set(self.solution).symmetric_difference(set(list(range(num_bikes)))))
-
-        if (np.random.uniform() <= new_gene_rate) and (len(choices) != 0):
-            new_gene_loc = np.random.randint(0, len(self.solution))
-            new_gene = np.random.choice(choices)
+            choices = list(set(list(range(1, num_bikes+1))) - set(self.solution))
+     
+            if (np.random.uniform() <= mutation_rate):
+                if len(choices) != 0:
+                    new_allele = np.random.choice(choices)
+                    self.mutations.append([i, self.solution[i], new_allele])
+                    self.solution[i] = new_allele
+                else:
+                    swap_idx_1 = np.random.choice(list(range(1, len(self.solution) - 1)))
+                    swap_idx_2 = np.random.choice(list(range(1, len(self.solution) - 1)))
+                    
+                    gene_1 = self.solution[swap_idx_1]
+                    gene_2 = self.solution[swap_idx_2]
+                    
+                    self.solution[swap_idx_1] = gene_2
+                    self.solution[swap_idx_2] = gene_1
+                    self.swaps.append([gene_1, gene_2])
             
-            self.insertions.append([new_gene_loc, new_gene])
+            
+    def new_gene(self, new_gene_rate = None):
+
+        if new_gene_rate is None:
+            new_gene_rate = 1/len(self.solution)
+
+        choices = list(set(list(range(1, num_bikes+1))) - set(self.solution))
+        
+        if (np.random.uniform() <= new_gene_rate) and (len(choices) > 0):
+            new_gene_loc = np.random.randint(1, len(self.solution)) # can't insert into first or last
+            new_gene = np.random.choice(choices)
+ 
+            self.insertions.append([(new_gene_loc, new_gene)])
             self.solution = np.insert(self.solution, obj = new_gene_loc, values = new_gene)
+            self.solution_size += 1
     
     def get_distance(self, dist_matrix):
         self.dist = 0
@@ -82,8 +116,8 @@ class Algorithm():
             current_loc = self.solution[i]
             next_loc = self.solution[i+1]
             
-            self.dist = self.dist + dist_matrix[current_loc, next_loc]
-            current_loc = self.solution[i]
+            self.dist = self.dist + dist_matrix[(dist_matrix['from'] == current_loc) & (dist_matrix['to'] == next_loc)].dist.item()
+            #self.dist = self.dist + dist_matrix[current_loc, next_loc]
         
         return self.dist
 
